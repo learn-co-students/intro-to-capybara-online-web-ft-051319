ecd70873379cb3f311035a040406fe26fe18373d
Kane Koo <kanekoo@gmail.com>
2019-07-04 20:14:59 -0400

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,11 @@
 class Application < Sinatra::Base
   # Write your code here!
+  get '/' do
+   erb :index
+  end
+  # when controller gets a POST request /greet (see form in index.erb)
+  post '/greet' do
+    erb :greet
+  end
 
 end
